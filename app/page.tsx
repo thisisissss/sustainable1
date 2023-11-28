0d521de141a48b6b16ae4af174637244184ed19f
thisisissss <somyown@proton.me>
2023-11-28 15:17:34 +0700

    info page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="/"
+            href="/info"
             rel="noopener noreferrer"
           >
             Info
@@ -30,7 +30,7 @@ export default function Home() {
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/logo-gg.svg"
-          alt="Next.js Logo"
+          alt="sgg logo"
           width={280}
           height={37}
           priority
