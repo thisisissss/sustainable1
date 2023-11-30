c01e48337dd59a6708172e47fc85abd8244b68f0
thisisissss <somyown@proton.me>
2023-11-30 15:16:04 +0700

    media

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,21 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+           
+            <div className="fixed bottom-0 left-0 right-0 m-4 flex justify-between items-center w-full lg:static lg:bg-none">
           <Link href="/info">
-            <div className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
+          <div className="flex w-full justify-center lg:static lg:h-auto lg:bg-none">
               Info
-            </div>
+              </div>
           </Link>
-        </div>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+     
           <Link href="/contact">
-            <div className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
+            <div className="flex place-items-center gap-4x p-8 lg:pointer-events-auto lg:p-0">
               Contact Us
             </div>
           </Link>
-        </div>
-      </div>
+          </div>
+   
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
