295b990d49964f49ad8665c8bb3344c3e0dc38ae
thisisissss <somyown@proton.me>
2023-12-07 20:45:07 +0700

    inquiry request

---
+++ pages/api/inquiry.js
@@ -0,0 +1,34 @@
+import nodemailer from 'nodemailer';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { name, phone, pageIdentifier } = req.body;
+
+    // Initialize Nodemailer transporter
+    const transporter = nodemailer.createTransport({
+      // Transport configuration (e.g., SMTP server details)
+      // This could be Gmail, SendGrid, etc., depending on your preference
+    });
+
+    // Construct the email content
+    const text = `New Inquiry from ${name} (Phone: ${phone}, Page: ${pageIdentifier})`;
+
+    try {
+      // Send an email
+      await transporter.sendMail({
+        from: 'your-email@example.com', // Your email address
+        to: 'somyown@yahoo.com', // Where you want to receive the inquiries
+        subject: 'New Customer Inquiry',
+        text: text,
+      });
+
+      res.status(200).json({ message: 'Inquiry submitted successfully!' });
+    } catch (error) {
+      console.error('Error sending email:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
--- pages/soap.tsx
+++ pages/soap.tsx
@@ -1,16 +1,56 @@
 // pages/index.js
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 export default function Soap() {
   const router = useRouter();
+
+  const [isPopupOpen, setPopupOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+  });
 
   const handleBackClick = () => {
     router.push("/"); // Or router.back() to go back in history
   };
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = {
+        name: formData.name,
+        phone: formData.phone,
+        pageIdentifier: "SoapPage", // or any other identifier you wish to use
+      };
+        // Send a POST request to the API route
+  try {
+    const response = await fetch('/api/inquiry', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+      const responseData = await response.json();
+      console.log(responseData.message);
+      setPopupOpen(false); // Close the popup after successful submission
+    } else {
+      // Handle HTTP errors
+      console.error("Failed to submit inquiry", response.status);
+    }
+  } catch (error) {
+    console.error("Error submitting form", error);
+  }
+};
   return (
     <div
       className="flex flex-col items-center justify-center min-h-screen py-2"
@@ -43,73 +83,130 @@ export default function Soap() {
           <div className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
             <Image
               src="https://i.imgur.com/V9gjKHh.jpg"
-              alt="Learn Next.js"
+              alt="Soap"
               width={256}
               height={144}
+              objectFit="cover"
               layout="responsive"
             />
-
-            <a className="inline-flex items-center mt-4 py-2 px-3 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button
+              className="inline-flex items-center mt-4 py-2 px-3 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={() => setPopupOpen(true)}
+            >
               Start Inquiry
-            </a>
+            </button>
           </div>
         </div>
       </main>
-     
-          <div className="text-center m-16 px-10 lg:px-32 justify-around bg-white rounded-lg border border-gray-200  shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <p className="mt-12 mb-4">
-              Embrace the gentle touch of nature with Pure Bliss Natural Soap.
-              Our commitment to purity begins with a steadfast refusal to use
-              petroleum-derived products like vaseline, ensuring your skin is
-              nurtured without compromising its natural balance. Our soap is a
-              testament to the power of simplicity, crafted with only the finest
-              natural ingredients. Unlike vaseline and other petroleum-based
-              products, which can clog pores and disrupt skin&apos;s natural
-              function, our natural soap offers a harmonious blend of nurturing
-              elements.
-            </p>
-            <h2 className="text-2xl font-semibold">
-              The Science of Petroleum Products and Skin Health:
-            </h2>
-            <p className="mt-4 mb-4">
-              Petroleum-based products, such as vaseline, are known to create a
-              barrier on the skin. This barrier can trap dirt and bacteria,
-              leading to clogged pores, skin irritation, and potentially
-              exacerbating skin conditions. Furthermore, the occlusive nature of
-              petroleum can disrupt the skin&apos;s ability to breathe and
-              regulate itself, potentially leading to imbalances in skin
-              moisture and health.
-            </p>
-            <h2 className="text-2xl font-semibold">
-              Why Natural Ingredients Matter:
-            </h2>
-            <p className="mt-4 mb-4">
-              In contrast, Pure Bliss Natural Soap features a blend of cocoa
-              butter, virgin oils, and NaOH lye. Cocoa butter, renowned for its
-              hydrating and nourishing properties, works in harmony with
-              carefully selected virgin oils to replenish and rejuvenate your
-              skin. The NaOH lye, essential in the soap-making process,
-              undergoes a transformation when combined with oils, resulting in a
-              product that is absolutely harmless and gentle on the skin.
-            </p>
-            <h2 className="text-2xl font-semibold">The Magic of Pure Bliss:</h2>
-            <p className="mt-4 mb-4">
-              This fusion of natural ingredients creates a luxurious lather that
-              cleanses effectively while respecting the skin&apos;s natural
-              barrier. The absence of artificial fragrances further ensures that
-              your skin enjoys the purest form of care, making Pure Bliss
-              Natural Soap a genuine celebration of nature&apos;s magic.
-              Experience the allure of simplicity and purity with Pure Bliss
-              Natural Soap – where every lather is a step closer to
-              nature&apos;s embrace.
-            </p>
-            <p className="mt-4 mb-12">
-              Join us on a journey of taste, tradition, and natural goodness.
-              Experience the difference with our artisanal cheeses, where every
-              bite tells a story of quality, purity, and a gentle touch.
-            </p>
+      <div className="relative text-center m-16 px-10 lg:px-32 justify-around rounded-lg border border-gray-200 shadow-md dark:border-gray-700">
+        <Image
+          src="https://i.imgur.com/2yz6DIr.png"
+          alt="Background"
+          layout="fill"
+          objectFit="cover"
+          style={{ opacity: 0.4 }}
+        />
+        <div className="relative z-10 p-4">
+          <p className="mt-12 mb-4 from-stone-50">
+            Embrace the gentle touch of nature with Pure Bliss Natural Soap. Our
+            commitment to purity begins with a steadfast refusal to use
+            petroleum-derived products like vaseline, ensuring your skin is
+            nurtured without compromising its natural balance. Our soap is a
+            testament to the power of simplicity, crafted with only the finest
+            natural ingredients. Unlike vaseline and other petroleum-based
+            products, which can clog pores and disrupt skin&apos;s natural
+            function, our natural soap offers a harmonious blend of nurturing
+            elements.
+          </p>
+          <h2 className="text-2xl font-semibold">
+            The Science of Petroleum Products and Skin Health:
+          </h2>
+          <p className="mt-4 mb-4">
+            Petroleum-based products, such as vaseline, are known to create a
+            barrier on the skin. This barrier can trap dirt and bacteria,
+            leading to clogged pores, skin irritation, and potentially
+            exacerbating skin conditions. Furthermore, the occlusive nature of
+            petroleum can disrupt the skin&apos;s ability to breathe and
+            regulate itself, potentially leading to imbalances in skin moisture
+            and health.
+          </p>
+          <h2 className="text-2xl font-semibold">
+            Why Natural Ingredients Matter:
+          </h2>
+          <p className="mt-4 mb-4">
+            In contrast, Pure Bliss Natural Soap features a blend of cocoa
+            butter, virgin oils, and NaOH lye. Cocoa butter, renowned for its
+            hydrating and nourishing properties, works in harmony with carefully
+            selected virgin oils to replenish and rejuvenate your skin. The NaOH
+            lye, essential in the soap-making process, undergoes a
+            transformation when combined with oils, resulting in a product that
+            is absolutely harmless and gentle on the skin.
+          </p>
+          <h2 className="text-2xl font-semibold">The Magic of Pure Bliss:</h2>
+          <p className="mt-4 mb-4">
+            This fusion of natural ingredients creates a luxurious lather that
+            cleanses effectively while respecting the skin&apos;s natural
+            barrier. The absence of artificial fragrances further ensures that
+            your skin enjoys the purest form of care, making Pure Bliss Natural
+            Soap a genuine celebration of nature&apos;s magic. Experience the
+            allure of simplicity and purity with Pure Bliss Natural Soap – where
+            every lather is a step closer to nature&apos;s embrace.
+          </p>
+          <p className="mt-4 mb-12">
+            Join us on a journey of taste, tradition, and natural goodness.
+            Experience the difference with our artisanal cheeses, where every
+            bite tells a story of quality, purity, and a gentle touch.
+          </p>
+        </div>
+      </div>
+      {isPopupOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 overflow-y-auto h-full w-full"
+          id="my-modal"
+        >
+          <div className="relative top-20 mx-auto p-10 border w-96 shadow-lg rounded-md bg-white">
+            <form className="mt-2" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={formData.name}
+                onChange={handleInputChange}
+                className="w-full p-2 border rounded mb-3"
+                required
+              />
+
+              <input
+                type="tel"
+                name="phone"
+                placeholder="Phone Number"
+                value={formData.phone}
+                onChange={handleInputChange}
+                className="w-full p-2 border rounded mb-3"
+                required
+              />
+              <input type="hidden" name="pageIdentifier" value="SoapPage" />
+
+              <div className="mt-8 flex justify-center space-x-4">
+                <button
+                  type="submit"
+                  className="px-4 py-2 bg-purple-600 text-white rounded hover:bg-purple-700"
+                >
+                  Contact request
+                </button>
+              </div>
+            </form>
+            <div className="mt-2 flex justify-center space-x-4">
+              <button
+                className="text-gray-500"
+                onClick={() => setPopupOpen(false)}
+              >
+                Close
+              </button>
+            </div>
           </div>
         </div>
-
+      )}
+    </div>
   );
 }
